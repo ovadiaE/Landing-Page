6679f4e7aa2bba5afc9fe0657054a3020c32edba
ovadiaE <ovadia.esq@gmail.com>
2022-01-30 01:09:19 +0200

    first portion of about and svgs

---
+++ landing-page/src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+import About from "./components/About";
+function App() {
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font"> 
+    <About/>
+    </main>
+  );
+}
+
+export default App;
---
+++ landing-page/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: [  "./src/**/*.{js,jsx}"],
+  theme: {
+    screens: {'md':'769px'},
+    extend: {},
+  },
+  plugins: [],
+}
